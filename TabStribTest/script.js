8603bc93ff41bf6686fa3c227370052da9d3b9f7
Kristian D. Dimitrov <kristiand.dimitrov@telerik.com>
2016-04-04 12:04:33 +0000

    Changes made by Kristian D. Dimitrov (kristiand.dimitrov@telerik.com).

--- TabStribTest/script.js
+++ TabStribTest/script.js
@@ -1,2 +1,22 @@
-// Code goes here
+(function(angular) {
+  'use strict';
+angular.module('docsTemplateUrlDirective', [])
+  .controller('Controller', ['$scope', function($scope) {
+    $scope.customer = {
+      name: 'Naomi',
+      address: '1600 Amphitheatre'
+    };
+  }])
+  .directive('myCustomer', function() {
+    return {
+		replace: true,
+      templateUrl: 'my-customer.html'
+    };
+  });
+})(window.angular);
 
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
